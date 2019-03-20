2b4f9dcbb24fc4a0f032d137ca99288a585cecde
Isaac142 <embramaze@gmail.com>
2019-03-20 19:34:04 +1100

    week5

---
+++ GP_Week_5/Assets/Scripts/DoorControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorControl : MonoBehaviour 
+{
+    //declare animator
+    Animator anim;
+    int open = Animator.StringToHash("Doors");
+
+    void Start()
+	{
+        //assign animator
+        anim = GetComponent<Animator>();
+
+    }
+
+    //on trigger enter, test if the collider is tagged "Player" and set trigger "Open"
+
+
+
+    //on trigger exit, test if the collider is tagged "Player" and set trigger "Close"
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.CompareTag("Player"))
+        //{
+        //    other.gambeObject.anim.SetTrigger(open);
+        //}
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+}
---
+++ GP_Week_5/Assets/Scripts/Interaction.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interaction : MonoBehaviour
+{
+
+    public GameObject fanModel;
+    public GameObject lightModel;
+    public Light lightColor;
+    public ParticleSystem particleSystem1;
+    public ParticleSystem particleSystem2;
+    public Color changingColor;
+    //declare public variables for light, lightModel, particle systems, fan model
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if fan rotation is on, transform.rotate
+
+        //if mouse button is down
+
+        //setup ray from camera to clicked mouseposition.
+
+        //if Physics.Raycast
+        //https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+        // Look for Raycast with Input.mousePosition
+
+        //    //if ray hits game object tagged "Light"
+
+        //    //change light model material to green
+        //    //change point light color to green
+        //    //SetActive both particle systems
+        //    //turn on rotation (called in main Update function)
+        //}
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                print(hit.collider.name);
+                lightModel.GetComponent<Renderer>().material.color = changingColor;
+                lightColor.color = Color.green;
+                particleSystem1.gameObject.SetActive(true);
+                particleSystem2.gameObject.SetActive(true);
+                //fanModel.transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+            }
+
+            Debug.DrawRay(transform.position, new Vector3(), Color.green);
+        }
+    }
+}
