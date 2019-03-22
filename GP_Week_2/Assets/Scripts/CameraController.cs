66fcb690321ca77ed2ed82d389e49d5ad24ae048
Isaac142 <embramaze@gmail.com>
2019-03-22 14:17:36 +1100

    week2

---
+++ GP_Week_2/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player; //Creating a GameObject to control and play around with it, rather than messing around with the model.
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
